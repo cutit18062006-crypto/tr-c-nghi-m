3a9572fc650e72804261f9c10c6f5c073dd9586b
Dung Nguyen <cutit18062006@gmail.com>
2026-07-19 16:04:21 +0700

    feat: Thêm SPA routing - URL /lambaithi cho trang Làm Bài Thi

--- server.js
+++ server.js
@@ -49,6 +49,10 @@ app.post('/upload', upload.single('image'), (req, res) => {
     });
 });
 
+app.get('/lambaithi', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.listen(port, () => {
     console.log(`Ứng dụng đang chạy tại: http://localhost:${port}`);
 });
